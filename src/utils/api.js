b58627bb859a8767fcdb300d27db9a4b4d191cd7
Shiva-Ydv55 <sivaram@visualpathit.com>
2026-03-27 11:53:20 +0530

    latest code

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5001/api';
+const BASE_URL = 'https://supportsmartschoolserp.com/api';
+//const BASE_URL = 'http://localhost:5001/api';
 
 const getHeaders = () => {
     const token = localStorage.getItem('token');
